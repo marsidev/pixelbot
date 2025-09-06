c36b0dfafbcf7c891ac8944e51863be48acd1230
Luis Marsiglia <marsiglia.business@gmail.com>
2025-09-05 21:39:02 -0400

    chore: change bumpp config

--- bump.config.ts
+++ bump.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "bumpp";
 
 export default defineConfig({
   files: ["package.json", "manifest.json", "updates.json"],
-  commit: "v%s",
+  commit: "chore: release v%s",
   tag: "v%s",
   push: true,
   confirm: true,
